bf463b82562948ab4a28e951e3903176b2dae518
Ioannis Kosmidis <i.kosmidis@ucl.ac.uk>
2017-03-21 18:52:47 +0000

    added a few more tests

---
+++ tests/testthat/test-correction.R
@@ -0,0 +1,24 @@
+context("implementation of bias correction")
+
+library("enrichwith")
+
+## A Gamma example, from McCullagh & Nelder (1989, pp. 300-2)
+clotting <- data.frame(
+    u = c(5,10,15,20,30,40,60,80,100, 5,10,15,20,30,40,60,80,100),
+    conc = c(118,58,42,35,27,25,21,19,18,69,35,26,21,18,16,13,12,12),
+    lot = factor(c(rep(1, 9), rep(2, 9))))
+
+mod <- glm(conc ~ lot*log(u), data = clotting, family = Gamma)
+emod <- enrich(mod, with = "all")
+
+bias_fun <- get_bias_function(emod)
+
+coefs <- coef(emod, model = "mean")
+disp <- coef(emod, model = "dispersion")
+coefs_bc <- c(coefs, disp) - bias_fun(coefs, disp)
+
+mod_bc <- glm(conc ~ lot*log(u), data = clotting, family = Gamma, method = "brglmFit", type = "correction")
+
+test_that("bias corrected estimates computed using enrichwith are the same as those when having type = 'correction' in brglmFit", {
+    expect_equal(coefs_bc, c(coef(mod_bc, model = "full")), check.attributes = FALSE, tolerance = 1e-10)
+})
