92611c7bad1e25fac56aec303bef997b2bbafca4
euloge clovis kenne pagui <kenne@stat.unipd.it>
2017-04-14 11:44:59 +0200

    I added the function AS_median_adjustment

--- R/brglmFit.R
+++ R/brglmFit.R
@@ -351,10 +351,37 @@ brglmFit <- function (x, y, weights = rep(1, nobs), start = NULL, etastart = NUL
         }
         with(fit, {
             if (level == 0) {
-                0
+                R_matrix <- qr.R(qr_decomposition)
+                info <- precision * crossprod(R_matrix)
+                inverse_info <- try(dispersion * tcrossprod(solve(R_matrix)))
+                nu_r_s_t <- nu_r_st <- array(0,c(nvars,nvars,nvars))
+                k1 <- k2 <- k3 <- b.beta <- rep(NA,nvars)
+                for (r in 1:nvars)
+                {
+                  nu_r_s_t[r,,] <- t(x)%*%((working_weights*d1mus*d1varmus*x[,r]/varmus)*x)
+                  nu_r_st[r,,] <- -t(x)%*%((working_weights*d1mus*(d1varmus/varmus-d2mus/d1mus^2)*x[,r])*x)
+                }
+                k2 <- 1/diag(inverse_info)
+                
+                for (r in 1:nvars) 
+                {
+                  sum_s1 <- rep(0,nvars)
+                  sum_s3 <- rep(0,nvars)
+                  nu.tu <- inverse_info-outer(inverse_info[,r]*k2[r],inverse_info[,r])
+                  for (s in 1:nvars){
+                    sum_s1[s] <- sum(diag(nu.tu%*%(nu_r_s_t[s,,]+nu_r_st[s,,])))
+                    sum_s3[s] <- sum((inverse_info[r,]%*%nu_r_s_t[s,,])*inverse_info[r,])
+                  }
+                  barb1r <- sum(sum_s1*inverse_info[r,])
+                  barb2r <- k2[r]*sum(sum_s3*inverse_info[r,])
+                  b.beta[r]<- barb1r/2+barb2r/6
+                }
+                return(info%*%b.beta)
             }
             if (level == 1) {
-                0
+                  s1 <- sum(weights^3 * d3afuns, na.rm = TRUE)
+                  s2 <- sum(weights^2 * d2afuns, na.rm = TRUE)
+                  return(nvars/(2*dispersion) + s1/(6*dispersion^2*s2))
             }
         })
     }
