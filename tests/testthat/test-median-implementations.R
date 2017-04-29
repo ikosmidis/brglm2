cd0baf2f01ff6a7b64cbf56c517e737c5928efc0
Ioannis Kosmidis <i.kosmidis@ucl.ac.uk>
2017-04-29 19:13:38 +0100

    Proper acknowldgement to M. Zambelli UG thesis

--- tests/testthat/test-median-implementations.R
+++ tests/testthat/test-median-implementations.R
@@ -1,5 +1,8 @@
-context("comparison for Poisson with mbrpr from the unpublished undergraduate 
-         thesis of M. Zambelli (2016) of  the University of Padova ")
+context("comparison for Poisson with mbrpr ")
+
+## mbrpr is supplied under the permission of M. Zambelli, and is part
+## of his unpublished undergraduate thesis submitted in 2016 at the
+## Department of Statisitcal Science, University of Padova
 
 source(system.file("mbrpr", "mbrpr.R", package = "brglm2"))
 
@@ -22,23 +25,4 @@ test_that("MBR estimates and std. errors from brglmFit and from mbrpr are the sa
   c2 <- cbind(br2$coefficients,sqrt(diag(br2$InfoInv)))
   expect_equal(c1,c2, tolerance = tol)
 })
-
-## ## mbest::firthglm.fit crashes for this example
-## ## A Gamma example, from McCullagh & Nelder (1989, pp. 300-2)
-## clotting <- data.frame(
-##     u = c(5,10,15,20,30,40,60,80,100, 5,10,15,20,30,40,60,80,100),
-##     conc = c(118,58,42,35,27,25,21,19,18,69,35,26,21,18,16,13,12,12),
-##     lot = factor(c(rep(1, 9), rep(2, 9))))
-## mod <- glm(conc ~ lot*log(u), data = clotting, family = Gamma())
-
-## tol <- sqrt(.Machine$double.eps)
-## test_that("BR estimates from brglmFit and from mbest::firthglm.fit are the same for gamma", {
-##     br1 <- update(mod, method = "firthglm.fit")
-##     br2 <- update(mod, method = "brglmFit")
-##     c1 <- coef(br1)
-##     c2 <- coef(br2)
-##     c2 <- c2[names(c1)]
-##     expect_equal(c1,c2, tolerance = tol)
-## })
-
 
