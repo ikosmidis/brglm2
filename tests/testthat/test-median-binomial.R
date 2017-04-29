8070ead2d56508af5c8b5038d77d8dba4caf040b
Ioannis Kosmidis <i.kosmidis@ucl.ac.uk>
2017-04-29 20:03:27 +0100

    Tidied up test

--- tests/testthat/test-median-binomial.R
+++ tests/testthat/test-median-binomial.R
@@ -6,36 +6,33 @@ data("endometrial",package = "brglm2")
 
 links <- lapply(c("logit", "probit", "cloglog", "cauchit"), make.link)
 
-
 tol <- 1e-10
-for (l in seq_along(links)) {
-    lizardsMBRlegacy <- mbrglm::mbrglm(cbind(grahami, opalinus) ~ height + diameter +
-                                      light + time, family = binomial(links[[l]]), data=lizards,
-                                    method = "mbrglm.fit",
-                                    control.mbrglm = mbrglm::mbrglm.control(mbr.epsilon = 1e-10, mbr.maxit = 1000))
-    lizardsMBR <- glm(cbind(grahami, opalinus) ~ height + diameter +
-                       light + time, family = binomial(links[[l]]), data=lizards,
-                     method = "brglmFit", epsilon = 1e-10, maxit = 1000,type="AS_median")
+mbrglmControl <- mbrglm::mbrglm.control(mbr.epsilon = 1e-10, mbr.maxit = 1000)
 
-    endoMBRlegacy <- mbrglm::mbrglm(HG~NV+PI+EH, family = binomial(links[[l]]), data=endo,method = "mbrglm.fit",
-                                    control.mbrglm = mbrglm::mbrglm.control(mbr.epsilon = 1e-10, mbr.maxit = 1000))
+for (l in seq_along(links)) {
+
+    ## Lizards
+    lizardsFormula <- cbind(grahami, opalinus) ~ height + diameter + light + time
+    lizardsMBRlegacy <- mbrglm::mbrglm(lizardsFormula, family = binomial(links[[l]]), data = lizards,
+                                       method = "mbrglm.fit",
+                                       control.mbrglm = mbrglmControl)
+    lizardsMBR <- glm(lizardsFormula, family = binomial(links[[l]]), data = lizards,
+                     method = "brglmFit", type="AS_median", epsilon = 1e-10, maxit = 1000)
+
+    ## Endometrial
+    endoFormula <- HG ~ NV + PI + EH
+    endoMBRlegacy <- mbrglm::mbrglm(endoFormula, family = binomial(links[[l]]), data = endometrial,
+                                    method = "mbrglm.fit",
+                                    control.mbrglm = mbrglmControl)
+
+    endoMBR <- glm(endoFormula, family = binomial(links[[l]]), data = endometrial,
+                   method = "brglmFit", type = "AS_median", epsilon = 1e-10, maxit = 1000)
+
     c1 <- coef(summary(endoMBRlegacy))
-    endoMBR <- glm(HG~NV+PI+EH, family = binomial(links[[l]]), data=endo,
-                     method = "brglmFit", epsilon = 1e-10, maxit = 1000,type="AS_median")
     c2 <- coef(summary(endoMBR))
-    test_that(paste("glm with brglm.fit method  and mbrglm return the same coefficients for the lizards when link is", links[[l]]$name), {
-      expect_equal(coef(lizardsMBR), coef(lizardsMBRlegacy), tolerance = tol)
-      expect_equal(c1,c2, tolerance = tol)
+
+    test_that(paste("glm with brglmFit method and mbrglm return the same coefficients when link is", links[[l]]$name), {
+        expect_equal(coef(lizardsMBR), coef(lizardsMBRlegacy), tolerance = tol)
+        expect_equal(c1, c2, tolerance = tol)
     })
 }
-
-## Performance comparisons BR versus ML
-## link1 <- "cauchit"
-## system.time(replicate(100, {lizardsBR <- glm(cbind(grahami, opalinus) ~ height + diameter + light + time, family = binomial(link1), data=lizards, method = "brglmFit", epsilon = 1e-10, maxit = 1000, type = "ML")}))
-
-## system.time(replicate(100, {lizardsBR <- glm(cbind(grahami, opalinus) ~ height + diameter + light + time, family = binomial(link1), data=lizards, method = "glm.fit", epsilon = 1e-10, maxit = 1000)}))
-
-## link1 <- "cauchit"
-## system.time(replicate(100, {lizardsBR <- glm(cbind(grahami, opalinus) ~ height + diameter + light + time, family = binomial(link1), data=lizards, method = "brglmFit", epsilon = 1e-10, maxit = 1000)}))
-
-## system.time(replicate(100, {lizardsBR <- brglm(cbind(grahami, opalinus) ~ height + diameter + light + time, family = binomial(link1), data=lizards, br.epsilon = 1e-10, br.maxit = 1000)}))
