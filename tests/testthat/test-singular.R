90452aa4557848faf6c3f689101c6d970d3bdb3a
Ioannis Kosmidis <i.kosmidis@ucl.ac.uk>
2017-10-12 16:15:02 +0100

    testing start and singular

---
+++ tests/testthat/test-singular.R
@@ -0,0 +1,17 @@
+context("test singular.ok argument")
+
+
+## A Gamma example, from McCullagh & Nelder (1989, pp. 300-2)
+clotting <- data.frame(
+    u = c(5,10,15,20,30,40,60,80,100, 5,10,15,20,30,40,60,80,100),
+    conc = c(118,58,42,35,27,25,21,19,18,69,35,26,21,18,16,13,12,12),
+    lot = factor(c(rep(1, 9), rep(2, 9))))
+mod <- glm(conc ~ lot*log(u) + I(2*log(u)), data = clotting, family = Gamma)
+
+test_that("brglmFit returns an error if singular.ok = TRUE", {
+    X <- model.matrix(mod)
+    Y <- mod$y
+    expect_error(brglm_fit(X, Y, family = Gamma(), singular.ok = FALSE),
+                 regexp = "singular fit encountered")
+    expect_true(is.na(coef(brglm_fit(X, Y, family = Gamma(), singular.ok = TRUE))["I(2 * log(u))"]))
+})
---
+++ tests/testthat/test-start.R
@@ -0,0 +1,18 @@
+context("test starting values")
+
+
+## A Gamma example, from McCullagh & Nelder (1989, pp. 300-2)
+clotting <- data.frame(
+    u = c(5,10,15,20,30,40,60,80,100, 5,10,15,20,30,40,60,80,100),
+    conc = c(118,58,42,35,27,25,21,19,18,69,35,26,21,18,16,13,12,12),
+    lot = factor(c(rep(1, 9), rep(2, 9))))
+mod <- glm(conc ~ lot*log(u), data = clotting, family = Gamma, epsilon = 1e-10, maxit = 1000)
+
+mod1 <- update(mod, method = "brglmFit", start = c(coef(mod)*0.9, 1), epsilon = 1e-10, maxit = 1000)
+mod2 <- update(mod, method = "brglmFit", start = c(coef(mod)*0.9), epsilon = 1e-10, maxit = 1000)
+
+tol <- 1e-04
+test_that("start argument is passed correctly in brglmFit", {
+    expect_equal(coef(mod), coef(mod1), tolerance = tol)
+    expect_equal(coef(mod), coef(mod2), tolerance = tol)
+})
