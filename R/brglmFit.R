455c40ed31cf8eb16482d3fe2352fb8e05aff23e
Ioannis Kosmidis <i.kosmidis@ucl.ac.uk>
2019-10-23 13:00:33 +0100

    Moved adjustments to separate script

--- R/brglmFit.R
+++ R/brglmFit.R
@@ -314,7 +314,8 @@ brglmFit <- function (x, y, weights = rep(1, nobs), start = NULL, etastart = NUL
                     dispersion = dispersion,
                     etas = etas,
                     mus = mus,
-                    scale_totals = scale_totals)
+                    scale_totals = scale_totals,
+                    weights = weights)
         mean_quantities <- function(out) {
             d1mus <- mu.eta(etas)
             d2mus <- d2mu.deta(etas)
@@ -458,132 +459,6 @@ brglmFit <- function (x, y, weights = rep(1, nobs), start = NULL, etastart = NUL
         list(dispersion = disp, dispersion_ML = dispML)
     }
 
-    AS_mean_adjustment <- function(pars, level = 0, fit = NULL) {
-        if (is.null(fit)) {
-            fit <- key_quantities(pars, y = y, level = level, qr = TRUE)
-        }
-        with(fit, {
-            if (level == 0) {
-                hatvalues <- hat_values(pars, fit = fit)
-                ## Use only observations with keep = TRUE to ensure that no division with zero takes place
-                return(.colSums(0.5 * hatvalues * d2mus/d1mus * x, nobs, nvars, TRUE))
-            }
-            if (level == 1) {
-                s1 <- sum(weights^3 * d3afuns, na.rm = TRUE)
-                s2 <- sum(weights^2 * d2afuns, na.rm = TRUE)
-                return((nvars - 2)/(2 * dispersion) + s1/(2 * dispersion^2 * s2))
-            }
-        })
-    }
-
-    AS_Jeffreys_adjustment <- function(pars, level = 0, fit = NULL) {
-        if (is.null(fit)) {
-            fit <- key_quantities(pars, y = y, level = level, qr = TRUE)
-        }
-        with(fit, {
-            if (level == 0) {
-                hatvalues <- hat_values(pars, fit = fit)
-                ## Use only observations with keep = TRUE to ensure that no division with zero takes place
-                return(2 * control$a * .colSums(0.5 * hatvalues * (2 * d2mus/d1mus - d1varmus * d1mus / varmus) * x, nobs, nvars, TRUE))
-            }
-            if (level == 1) {
-                s1 <- sum(weights^3 * d3afuns, na.rm = TRUE)
-                s2 <- sum(weights^2 * d2afuns, na.rm = TRUE)
-                return(2 * control$a * (-(nvars + 4)/(2 * dispersion) + s1/(2 * dispersion^2 * s2)))
-            }
-        })
-    }
-
-    
-    ## Implementation by Euloge Clovis Kenne Pagui, 20 April 2017 (kept here for testing)
-    ## AS_median_adjustment <- function(pars, level = 0, fit = NULL) {
-    ##     if (is.null(fit)) {
-    ##         fit <- key_quantities(pars, y = y, level = level, qr = TRUE)
-    ##     }
-    ##     with(fit, {
-    ##         if (level == 0) {
-    ##             R_matrix <- qr.R(qr_decomposition)
-    ##             info <- precision * crossprod(R_matrix)
-    ##             inverse_info <- try(dispersion * tcrossprod(solve(R_matrix)))
-    ##             nu_r_s_t <- nu_r_st <- array(0,c(nvars,nvars,nvars))
-    ##             k1 <- k2 <- k3 <- b.beta <- rep(NA,nvars)
-    ##             for (r in 1:nvars)
-    ##             {
-    ##               nu_r_s_t[r,,] <- t(x)%*%((working_weights*d1mus*d1varmus*x[,r]/varmus)*x)
-    ##               nu_r_st[r,,] <- -t(x)%*%((working_weights*d1mus*(d1varmus/varmus-d2mus/d1mus^2)*x[,r])*x)
-    ##             }
-    ##             k2 <- 1/diag(inverse_info)
-    ##             for (r in 1:nvars)
-    ##             {
-    ##               sum_s1 <- rep(0,nvars)
-    ##               sum_s3 <- rep(0,nvars)
-    ##               nu.tu <- inverse_info-outer(inverse_info[,r]*k2[r],inverse_info[,r])
-    ##               for (s in 1:nvars){
-    ##                 sum_s1[s] <- sum(diag(nu.tu%*%(nu_r_s_t[s,,]+nu_r_st[s,,])))
-    ##                 sum_s3[s] <- sum((inverse_info[r,]%*%nu_r_s_t[s,,])*inverse_info[r,])
-    ##               }
-    ##               barb1r <- sum(sum_s1*inverse_info[r,])
-    ##               barb2r <- k2[r]*sum(sum_s3*inverse_info[r,])
-    ##               b.beta[r]<- barb1r/2+barb2r/6
-    ##             }
-    ##             return(info%*%b.beta)
-    ##         }
-    ##         if (level == 1) {
-    ##               s1 <- sum(weights^3 * d3afuns, na.rm = TRUE)
-    ##               s2 <- sum(weights^2 * d2afuns, na.rm = TRUE)
-    ##               return(nvars/(2*dispersion) + s1/(6*dispersion^2*s2))
-    ##         }
-    ##     })
-    ## }
-
-    ## Implementation by Ioannis Kosmidis, 02 May 2017
-    AS_median_adjustment <- function(pars, level = 0, fit = NULL) {
-        if (is.null(fit)) {
-            fit <- key_quantities(pars, y = y, level = level, qr = TRUE)
-        }
-        with(fit, {
-            if (level == 0) {
-                hatvalues <- hat_values(pars, fit = fit)
-                R_matrix <- qr.R(qr_decomposition)
-                info_unscaled <- crossprod(R_matrix)
-                inverse_info_unscaled <- chol2inv(R_matrix)
-                ## FIXME: There is 1) definitely a better way to do this, 2) no time...
-                b_vector <- numeric(nvars)
-                for (j in seq.int(nvars)) {
-                    inverse_info_unscaled_j <- inverse_info_unscaled[j, ]
-                    vcov_j <- tcrossprod(inverse_info_unscaled_j) / inverse_info_unscaled_j[j]
-                    hats_j <- .rowSums((x %*% vcov_j) * x, nobs, nvars, TRUE) * working_weights
-                    b_vector[j] <- inverse_info_unscaled_j %*% .colSums(x * (hats_j * (d1mus * d1varmus / (6 * varmus) - 0.5 * d2mus/d1mus)), nobs, nvars, TRUE)
-                }
-                return(.colSums(0.5 * hatvalues * d2mus / d1mus * x, nobs, nvars, TRUE) +
-                       info_unscaled %*% b_vector)
-            }
-            if (level == 1) {
-                s1 <- sum(weights^3 * d3afuns, na.rm = TRUE)
-                s2 <- sum(weights^2 * d2afuns, na.rm = TRUE)
-                return(nvars/(2 * dispersion) + s1/(6 * dispersion^2 * s2))
-            }
-        })
-    }
-
-    AS_mixed_adjustment <- function(pars, level = 0, fit = NULL) {
-        if (is.null(fit)) {
-            fit <- key_quantities(pars, y = y, level = level, qr = TRUE)
-        }
-        with(fit, {
-            if (level == 0) {
-                hatvalues <- hat_values(pars, fit = fit)
-                ## Use only observations with keep = TRUE to ensure that no division with zero takes place
-                return(.colSums(0.5 * hatvalues * d2mus/d1mus * x, nobs, nvars, TRUE))
-            }
-            if (level == 1) {
-                s1 <- sum(weights^3 * d3afuns, na.rm = TRUE)
-                s2 <- sum(weights^2 * d2afuns, na.rm = TRUE)
-                return(nvars/(2 * dispersion) + s1/(6 * dispersion^2 * s2))
-            }
-        })
-    }
-
 
     ## compute_step_components does everything on the scale of the /transformed/ dispersion
     compute_step_components <- function(pars, level = 0, fit = NULL) {
@@ -627,7 +502,7 @@ brglmFit <- function (x, y, weights = rep(1, nobs), start = NULL, etastart = NUL
     }
 
     control <- do.call("brglmControl", control)
-
+    
     ## FIXME: Add IBLA
     adjustment_function <- switch(control$type,
                             "correction" = AS_mean_adjustment,
@@ -930,7 +805,7 @@ brglmFit <- function (x, y, weights = rep(1, nobs), start = NULL, etastart = NUL
                     ## Update betas
                     betas <- betas + slowit * 2^(-step_factor) * step_beta
                     ## Update zetas
-                    if (!no_dispersion & df_residual > 0) {
+                    if (!no_dispersion && df_residual > 0) {
                         transformed_dispersion <- eval(control$Trans)
                         transformed_dispersion <- transformed_dispersion + 2^(-step_factor) * step_zeta
                         dispersion <- eval(control$inverseTrans)
@@ -1023,8 +898,6 @@ brglmFit <- function (x, y, weights = rep(1, nobs), start = NULL, etastart = NUL
             betas[is.na(betas)] <- 0
             nvars <- nvars_all
         }
-##:ess-bp-start::browser@nil:##
-browser(expr=is.null(.ESSBP.[["@10@"]]));##:ess-bp-end:##
         
         ## If has_fixed_totals = TRUE, then scale fitted values before
         ## calculating QR decompositions, fitted values, etas,
@@ -1073,24 +946,6 @@ browser(expr=is.null(.ESSBP.[["@10@"]]));##:ess-bp-end:##
         if (df_residual == 0 & !no_dispersion) {
             dispersion <- NA_real_
         }
-
-        ## ## Estimate of first-order bias from the last iteration (so
-        ## ## not at the final value for the coefficients)
-        ## if (is_ML) {
-        ##     ## For now... To be set to calculate biases at a later version
-        ##     bias_betas <- bias_zeta <- NULL
-        ## }
-        ## else {
-        ##     bias_betas <- with(step_components_beta, -drop(inverse_info %*% adjustment))
-        ##     bias_zeta <- with(step_components_zeta, -drop(inverse_info %*% adjustment))
-        ##     bias_betas_all <- betas_all
-        ##     bias_betas_all[betas_names] <- bias_betas
-        ##     ## If correction has been requested then add estimated biases an attribute to the coefficients
-        ##     if (is_correction) {
-        ##         attr(betas_all, "biases") <- bias_betas_all
-        ##         attr(transformed_dispersion, "biases") <- bias_zeta
-        ##     }
-        ## }
     }
 
     ## Working weights
