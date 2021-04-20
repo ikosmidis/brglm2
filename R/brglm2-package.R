b6126f12f9bbcc0e42302074932ad80d02c3638d
Ioannis Kosmidis <i.kosmidis@ucl.ac.uk>
2021-04-20 18:12:07 +0100

    Added brnb documentation

--- R/brglm2-package.R
+++ R/brglm2-package.R
@@ -94,6 +94,9 @@
 #'
 NULL
 
+## NAMESPACE should have import(stats), import(Matrix)
+
+
 ## Suggestion by Kurt Hornik to avoid a warning related to the binding
 ## of n which is evaluated by family$initialize
 if (getRversion() >= "2.15.1") globalVariables(c("n", "lambda"))
