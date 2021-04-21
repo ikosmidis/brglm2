4d9dbc265e0e1aafcc5860cd6d9bc41144338f11
Ioannis Kosmidis <i.kosmidis@ucl.ac.uk>
2021-04-21 15:51:06 +0100

    Some housekeeping

--- src/init.c
+++ src/init.c
@@ -1,10 +1,6 @@
 #include <stdlib.h> // for NULL
 #include <R_ext/Rdynload.h>
 
-/* FIXME: 
-   Check these declarations against the C/Fortran source code.
-*/
-
 /* .C calls */
 extern void expectedValues(void *, void *, void *, void *, void *, void *, void *, void *);
 
